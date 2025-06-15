4b4f52616d1644e104277983780f5cccb4142f7d
da-ket <daket.duet@gmail.com>
2025-06-15 20:17:51 +0900

    streamlit: create self-introduction in text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("👽guess who I am")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+"I'm a full-stack growth marketer at a lifestyle app service(name:beep--"secret"), based in seoul, Korea.
+
+🤓 Some examples of projects I've worked on include:
+- Growth and CRM strategy.
+- Auditing and optimizing performance marketing campaigns (Search ads, Meta and Google ads, Moloco and etc)
+- Designing and operating data-driven, personalized CRM campaigns across push, IAM, and SMS.
+- Analyzing user journey using solutions (MMP, Braze, Amplitude, MySQL, Snowflake, etc)
+
+🍦 Soft skills
+- I'm a fast-learner, strategic thinker.
+
+📣 Language
+- Korean (Mother language🤭)
+- English 
+- Mandarin (Certified: hsk level 6, Business ITT, major in uni) 
+- tiny little japanese
+")
